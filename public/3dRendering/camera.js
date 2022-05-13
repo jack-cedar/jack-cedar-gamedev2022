e52e5c9e134ca5524c9b4682fc75f370a3563185
Jack Janousek-Weaver <78448269+jack-cedar@users.noreply.github.com>
2022-05-13 11:01:55 -0700

    Update camera.js

--- public/3dRendering/camera.js
+++ public/3dRendering/camera.js
@@ -1,26 +1,10 @@
 class camera{
     constructor()
     {
-        // Position of the camera
-        this.cameraPos = [0, 0, -3]
-
-        this.cameraTarget = [0, 0, 0]
-
-       
-
-        this.cameraDirection = 
-        [
-            [this.cameraPos[0]-this.cameraTarget[0]],
-            [this.cameraPos[1]-this.cameraTarget[1]],
-            [this.cameraPos[2]-this.cameraTarget[2]]
-        ]
         this.fov = 120
-  
         this.aspectRatio = (height / width)
-  
         this.near= 0.1
         this.far= 10000
-  
         this.view =
         [
             [this.fov*this.aspectRatio, 0, 0, 0],
