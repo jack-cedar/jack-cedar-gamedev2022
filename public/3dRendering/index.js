e2abe08afd0355d8157e1193738b4de4164b5e0c
Jack Janousek-Weaver <78448269+jack-cedar@users.noreply.github.com>
2022-05-13 11:06:58 -0700

    Update index.js

--- public/3dRendering/index.js
+++ public/3dRendering/index.js
@@ -8,7 +8,6 @@ ctx.translate(width/2, height/2);
 
 let c1 = new camera
 
-
 function getMatrix(points){
   var pointMatrix = [];
   for (i = 0; i < points.length; i++){
@@ -17,47 +16,19 @@ function getMatrix(points){
   }
   return(pointMatrix);
 }
+
 let obj01;
 let shape01;
 async function start()
 {
   obj01 = await _objParser("./sphere.obj")
   shape01 = new Mesh(obj01)
-
   update();
 }
-start()
-
-
 function update()
 {
-    
     ctx.clearRect(-width / 2, -height / 2, width, height);
-    
     shape01.update()
-    //shape02.update()
-    //testb.angleX += 0.01
     requestAnimationFrame(update);    
 }
-
-
-function sumz(polygons)
-{
-  let p = polygons
-  for(i = 0; i < p.length; i++)
-  {
-    let sum = 0;
-    let l = p[i].points.length
-    let j = 0
-    while(j < l)
-    {
-      sum += p[i].points[j].sz 
-      j++
-    }
-    p[i].zsum = sum
-  }
-}
-
-
-
-
+start()
